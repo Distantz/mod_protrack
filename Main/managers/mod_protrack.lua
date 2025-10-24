b601b9a0be80fcb246f6f0e64753f7443ef6bfce
Coppertine <37494321+Coppertine@users.noreply.github.com>
2025-10-24 23:52:58 +1000

    update it to ensure the ui can even show.
    it can't right now.. cus current branch is a crasher..

--- Main/managers/mod_protrack.lua
+++ Main/managers/mod_protrack.lua
@@ -110,6 +110,8 @@ end
 
 function protrackManager.StartEditMode(self, trackEditMode)
     logger:Info("Starting edit mode!")
+    --- TODO: Add Ui initialisation here
+
     self:ZeroData()
 
     logger:Info("Zeroed")
@@ -207,6 +209,8 @@ function protrackManager.StartTrackCamera(self)
     if not self.inCamera then
         Gizmo.SetMarkerGizmosVisible(false)
         Cam.StartRideCamera()
+        --- TODO: Add force ui show here
+        protrackManager.overlayUI:Show()
         self.inCamera = true
     end
 end
@@ -215,6 +219,8 @@ function protrackManager.StopTrackCamera(self)
     if self.inCamera then
         Gizmo.SetMarkerGizmosVisible(true)
         Cam.StopRideCamera()
+        --- TODO: Add force ui hide here
+        protrackManager.overlayUI:Hide()
         self.inCamera = false
     end
 end
