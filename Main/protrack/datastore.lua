967e5671de17d9066e56cea9d57f857f049ea824
Thomas <64567080+Distantz@users.noreply.github.com>
2025-10-25 20:56:44 +1300

    feat: add new funcs to datastore, clean up util

--- Main/protrack/datastore.lua
+++ Main/protrack/datastore.lua
@@ -11,30 +11,56 @@ local TransformQ = require("TransformQ")
 local Datastore = {}
 Datastore.tSimulationDelta = 0.1
 
-Datastore.tDatapoints = {}
+---@class TrackMeasurement
+---@field g any
+---@field transform any
+
+---@type TrackMeasurement[]|nil
+Datastore.tDatapoints = nil
 Datastore.trackEntityTransform = nil
 Datastore.trackWalkerOrigin = nil
 
+--- Returns whether the datastore has any data for display
+---@return boolean
 function Datastore.HasData()
     return Datastore.tDatapoints ~= nil and #Datastore.tDatapoints >= 2
 end
 
-function Datastore.SampleDatapointAtTime(time)
+--- Returns the timestamp for an index, which can be fractional.
+--- Indexes start at 1 and increase to the number of datapoints
+---@param floatIndex number
+---@return number
+function Datastore.GetTimeForFloatIndex(floatIndex)
+    return floatIndex * Datastore.tSimulationDelta
+end
+
+--- Returns the float index for a time, which can be fractional.
+--- Indexes start at 1 and increase to the number of datapoints.
+---@param time number
+---@return number
+function Datastore.GetFloatIndexForTime(time)
+    return time / Datastore.tSimulationDelta
+end
+
+--- Returns the floor of the float index for a time.
+--- Indexes start at 1 and increase to the number of datapoints.
+---@param time number
+---@return integer
+function Datastore.GetFloorIndexForTime(time)
+    return global.math.floor(Datastore.GetFloatIndexForTime(time))
+end
+
+--- Samples a datapoint at a float index.
+--- Will interpolate between datapoints between float indexes.
+---@param floatIndex number
+---@return TrackMeasurement|nil
+function Datastore.SampleDatapointAtFloatIndex(floatIndex)
     if not Datastore.HasData() then
         logger:Error("SampleDatapointAtTime requires datastore to have data! See Datastore.HasData().")
         return nil
     end
 
-    -- clamp time
-    if (time < 0) then
-        time = 0
-    end
-
     local numPts = #Datastore.tDatapoints
-
-    -- Convert into 0 -> 1 range
-    local floatIndex = time / Datastore.tSimulationDelta
-
     local floor = global.math.floor(floatIndex)
     local fractionalLerp = floatIndex - floor
 
@@ -57,8 +83,24 @@ function Datastore.SampleDatapointAtTime(time)
     }
 end
 
+--- Samples a datapoint at a time.
+--- Will interpolate between datapoints between float indexes.
+---@param time number
+---@return TrackMeasurement|nil
+function Datastore.SampleDatapointAtTime(time)
+    return Datastore.SampleDatapointAtFloatIndex(Datastore.GetFloatIndexForTime(time))
+end
+
+---Returns the total time length of the datastore
+---@return number
 function Datastore.GetTimeLength()
     return #Datastore.tDatapoints * Datastore.tSimulationDelta
+end
+
+---Returns the number of datapoints in the datastore
+---@return integer
+function Datastore.GetNumDatapoints()
+    return #Datastore.tDatapoints
 end
 
 return Datastore
