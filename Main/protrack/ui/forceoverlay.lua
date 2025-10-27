8701796075ad0128ee4f8fc34ecb1538726bea41
Coppertine <37494321+Coppertine@users.noreply.github.com>
2025-10-27 20:18:29 +1000

    Fix ui depth issue.
    It appeared above pause menu and end of day report.. it didn't look
    good.

--- Main/protrack/ui/forceoverlay.lua
+++ Main/protrack/ui/forceoverlay.lua
@@ -17,29 +17,29 @@ local ForceOverlay      = module(..., Object.subclass(GamefaceUIWrapper))
 local ObjectNew         = ForceOverlay.new
 
 function ForceOverlay:new(_fnOnReadyCallback)
-    logger:Info("New ForceOverlay")
-    local oNewForceOverlay = ObjectNew(ForceOverlay)
-    local tInitSettings = {
-        sViewName = "ProTrackUI",
-        sViewAddress = "coui://UIGameface/ProTrack_CamForceOverlay.html",
-        bStartEnabled = true,
-        fnOnReadyCallback = _fnOnReadyCallback,
-        nViewDepth = 1,
-        nViewHeight = 1920,
-        nViewWidth = 1080,
-        bRegisterWrapper = true
-    }
-    logger:Info("Attempting to Init Overlay UI")
-    oNewForceOverlay:Init(tInitSettings)
-    return oNewForceOverlay
+	logger:Info("New ForceOverlay")
+	local oNewForceOverlay = ObjectNew(ForceOverlay)
+	local tInitSettings = {
+		sViewName = "ProTrackUI",
+		sViewAddress = "coui://UIGameface/ProTrack_CamForceOverlay.html",
+		bStartEnabled = true,
+		fnOnReadyCallback = _fnOnReadyCallback,
+		nViewDepth = 0,
+		nViewHeight = 1920,
+		nViewWidth = 1080,
+		bRegisterWrapper = true
+	}
+	logger:Info("Attempting to Init Overlay UI")
+	oNewForceOverlay:Init(tInitSettings)
+	return oNewForceOverlay
 end
 
 function ForceOverlay:Show()
-    logger:Info("Showing Overlay")
-    self:TriggerEventAtNextAdvance("Show")
+	logger:Info("Showing Overlay")
+	self:TriggerEventAtNextAdvance("Show")
 end
 
 function ForceOverlay:Hide()
-    logger:Info("Hiding Overlay")
-    self:TriggerEventAtNextAdvance("Hide")
+	logger:Info("Hiding Overlay")
+	self:TriggerEventAtNextAdvance("Hide")
 end
